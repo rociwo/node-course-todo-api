0de012c70a61d842fba8b0e1ddfeae6dfd64817f
ROCIO <rocio.alfonso@cuatroochenta.com>
2018-01-02 16:30:16 +0100

    Edit for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://<db_username>:<s8db0Ois3dDD>@ds235827.mlab.com:35827/mongodb-node');
+mongoose.connect('mongodb://db_username:s8db0Ois3dDD@ds235827.mlab.com:35827/mongodb-node');
 
 module.exports = { mongoose };
