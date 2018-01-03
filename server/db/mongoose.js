44523be017a1ca3615fb439e963f76a3779c67c4
ROCIO <rocio.alfonso@cuatroochenta.com>
2018-01-03 10:31:14 +0100

    Update mongodb uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://db_username:s8db0Ois3dDD@ds235827.mlab.com:35827/mongodb-node');
+//mongoose.connect('mongodb://db_username:s8db0Ois3dDD@ds235827.mlab.com:35827/mongodb-node');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
 
 module.exports = { mongoose };
