d7d8e3d31c4234ec427e483cf0b96ed3dc83d15c
ROCIO <rocio.alfonso@cuatroochenta.com>
2018-01-03 16:34:37 +0100

    Setup reparate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
 //mongoose.connect('mongodb://db_username:s8db0Ois3dDD@ds235827.mlab.com:35827/mongodb-node');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports = { mongoose };
